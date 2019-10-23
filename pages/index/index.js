f4b2f32cdc584a1841c468230b84e605c65f9938
ickt_22_陈晟宁 <1185451124@qq.com>
2019-10-23 19:28:16 +0800

    miniprogram

---
+++ pages/index/index.js
@@ -0,0 +1,92 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navList: ""
+  },
+  getNavlist(){
+    let that = this;
+    wx.request({
+      url:"http://yapi.demo.qunar.com/mock/10950/navlist/navlist",
+      success(res){
+        console.log(res.data)
+        that.setData({
+          navList: res.data.data
+        })
+      }
+    })
+  },
+  pay(){
+    wx.requestPayment({
+      timeStamp: '',
+      nonceStr: '',
+      package: '',
+      signType: 'MD5',
+      paySign: '',
+      success(res) { 
+        console.log(res)
+      },
+      fail(res) { 
+        console.log(res)
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getNavlist()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
